177a3f00e618b673e3416bf5900050bcbb186c11
Yuri Torres <yuri.salgado.ft@gmail.com>
2025-12-11 15:27:35 -0300

    feat: Add public and privates properties (Customer) class

--- src/shop-demo-v2.0/Models/Customer.cs
+++ src/shop-demo-v2.0/Models/Customer.cs
@@ -2,6 +2,17 @@ namespace ShopDemo.Models
 {
     public class Customer
     {
-        
+        // private properties
+        private string _firstName;
+        private string _lastName;
+        private string _emailAddress;
+        private DateOnly _dateOfBirth;
+
+        // public properties
+        public int CustomerId { get; private set; }
+        public string FirstName => _firstName;
+        public string LastName => _lastName;
+        public string EmailAddress => _emailAddress;
+        public DateOnly DateOfBirth =>  _dateOfBirth;
     }
 }
