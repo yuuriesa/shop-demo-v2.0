0c7cdd2bb4b032a802423cfed1facdcd3ee69f14
Yuri Torres <yuri.salgado.ft@gmail.com>
2025-12-15 16:01:33 -0300

    feat: added Customer in IApplicationDbContext

--- src/shop-demo-v2.0/Data/IApplicationDbContext.cs
+++ src/shop-demo-v2.0/Data/IApplicationDbContext.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using ShopDemo.Models;
+
 namespace ShopDemo.Data
 {
     public interface IApplicationDbContext
     {
-        
+        DbSet<Customer> Customers { get; set; }
     }
 }
