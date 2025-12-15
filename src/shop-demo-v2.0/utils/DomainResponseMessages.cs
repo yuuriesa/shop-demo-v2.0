93918d432419ce42b69d122e428d8218480d54f9
Yuri Torres <yuri.salgado.ft@gmail.com>
2025-12-15 15:48:07 -0300

    feat: added MaximumOf40CharactersError error message in DomainResponseMessage class

--- src/shop-demo-v2.0/utils/DomainResponseMessages.cs
+++ src/shop-demo-v2.0/utils/DomainResponseMessages.cs
@@ -2,6 +2,6 @@ namespace ShopDemo.Utils
 {
     public class DomainResponseMessages
     {
-        
+        public const string MaximumOf40CharactersError = "Must have a maximum of 40 characters";
     }
 }
