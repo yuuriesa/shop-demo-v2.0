1f8cc64bee5ee7145d59d93013cf963b3b784e21
Yuri Torres <yuri.salgado.ft@gmail.com>
2025-12-20 22:28:37 -0300

    feat: Add GetAll method in IRepositoryBase

--- src/shop-demo-v2.0/Repository/IRepositoryBase.cs
+++ src/shop-demo-v2.0/Repository/IRepositoryBase.cs
@@ -1,7 +1,9 @@
+using ShopDemo.Utils;
+
 namespace ShopDemo.Repository
 {
     public interface IRepositoryBase<TEntity> where TEntity : class
     {
-        
+        public IQueryable<TEntity> GetAll(PaginationFilter paginationFilter);
     }
 }
