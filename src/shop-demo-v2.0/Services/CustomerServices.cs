ef4d7ef6027bf410224b195ac0a80cef4f99e7f6
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-01-02 17:30:23 -0300

    feat: add _repository and _dbcontext property to CustomerServices

--- src/shop-demo-v2.0/Services/CustomerServices.cs
+++ src/shop-demo-v2.0/Services/CustomerServices.cs
@@ -1,7 +1,11 @@
+using ShopDemo.Data;
+using ShopDemo.Repository;
+
 namespace ShopDemo.Services
 {
     public class CustomerServices : ICustomerServices
     {
-        
+        private readonly ICustomerRepository _repository;
+        private readonly ApplicationDbContext _dbContext;
     }
 }
