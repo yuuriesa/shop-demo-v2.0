ab7ca2f1e6f0bf2d6eca855ef7d24177943373ae
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-01-10 14:11:24 -0300

    feat: Add GetAll method to ICustomerServices

--- src/shop-demo-v2.0/Services/ICustomerServices.cs
+++ src/shop-demo-v2.0/Services/ICustomerServices.cs
@@ -1,7 +1,10 @@
+using ShopDemo.Models;
+using ShopDemo.Utils;
+
 namespace ShopDemo.Services
 {
     public interface ICustomerServices
     {
-        
+        public IQueryable<Customer> GetAll(PaginationFilter paginationFilter);
     }
 }
