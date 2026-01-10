6369334a9fbc5e95203714ddb1df4c03e251d475
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-01-10 13:15:38 -0300

    feat: Added CustomerPaginationError to DomainReponseMessages

--- src/shop-demo-v2.0/utils/DomainResponseMessages.cs
+++ src/shop-demo-v2.0/utils/DomainResponseMessages.cs
@@ -4,5 +4,6 @@ namespace ShopDemo.Utils
     {
         public const string MaximumOf40CharactersError = "Must have a maximum of 40 characters";
         public const string DateOfBirthError = "You cannot put the date with the day after today.";
+        public const string CustomerPaginationError = "The pagination parameters 'pageSize' and 'pageNumber' must be positive numbers. Check the values provided.";
     }
 }
