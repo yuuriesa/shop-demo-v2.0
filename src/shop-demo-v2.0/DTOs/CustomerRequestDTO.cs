e31522d9c12125ff96a77c66dcd8d6bbe60d36b1
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-01-11 14:34:10 -0300

    feat: Add validation to the last name propertie in CustomerRequestDTO

--- src/shop-demo-v2.0/DTOs/CustomerRequestDTO.cs
+++ src/shop-demo-v2.0/DTOs/CustomerRequestDTO.cs
@@ -5,9 +5,11 @@ namespace ShopDemo.DTOs
 {
     public class CustomerRequestDTO
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage = DomainResponseMessages.CustomerFirstNameIsRequired)]
+        [Required(ErrorMessage = DomainResponseMessages.CustomerFirstNameIsRequired)]
         [MaxLength(length: CustomerConstantsRules.MAXIMUM_CHARACTERS_FIRST_NAME, ErrorMessage = DomainResponseMessages.MaximumOf40CharactersError)]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = DomainResponseMessages.CustomerLastNameIsRequired)]
+        [MaxLength(length: CustomerConstantsRules.MAXIMUM_CHARACTERS_LAST_NAME, ErrorMessage = DomainResponseMessages.MaximumOf40CharactersError)]
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
         public DateOnly DateOfBirth { get; set; }
