cdce7261b22cbeffacef93d669174936060ea04a
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-01-11 14:43:18 -0300

    feat: Add validation to the email address propertie in CustomerRequestDTO

--- src/shop-demo-v2.0/DTOs/CustomerRequestDTO.cs
+++ src/shop-demo-v2.0/DTOs/CustomerRequestDTO.cs
@@ -5,12 +5,14 @@ namespace ShopDemo.DTOs
 {
     public class CustomerRequestDTO
     {
-        [Required(ErrorMessage = DomainResponseMessages.CustomerFirstNameIsRequired)]
+        [Required(ErrorMessage = DomainResponseMessages.CustomerFirstNameIsRequiredError)]
         [MaxLength(length: CustomerConstantsRules.MAXIMUM_CHARACTERS_FIRST_NAME, ErrorMessage = DomainResponseMessages.MaximumOf40CharactersError)]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = DomainResponseMessages.CustomerLastNameIsRequired)]
+        [Required(ErrorMessage = DomainResponseMessages.CustomerLastNameIsRequiredError)]
         [MaxLength(length: CustomerConstantsRules.MAXIMUM_CHARACTERS_LAST_NAME, ErrorMessage = DomainResponseMessages.MaximumOf40CharactersError)]
         public string LastName { get; set; }
+        [Required(ErrorMessage = DomainResponseMessages.CustomerEmailAddressIsRequiredError)]
+        [EmailAddress(ErrorMessage = DomainResponseMessages.CustomerEmailFieldIsNotAValid)]
         public string EmailAddress { get; set; }
         public DateOnly DateOfBirth { get; set; }
     }
