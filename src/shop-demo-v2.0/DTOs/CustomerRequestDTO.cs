c991da0f0e31d745d0f56b1414d418b8ebb274bd
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-01-11 13:58:04 -0300

    feat: Added properties to CustomerRequestDTO

--- src/shop-demo-v2.0/DTOs/CustomerRequestDTO.cs
+++ src/shop-demo-v2.0/DTOs/CustomerRequestDTO.cs
@@ -2,6 +2,9 @@ namespace ShopDemo.DTOs
 {
     public class CustomerRequestDTO
     {
-        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public DateOnly DateOfBirth { get; set; }
     }
 }
