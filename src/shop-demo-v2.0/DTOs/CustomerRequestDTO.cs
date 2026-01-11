d6637433eb993813ccfe0bf6ae0a262e5b26903b
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-01-11 14:25:43 -0300

    feat: Add validation to the first name propertie in CustomerRequestDTO

--- src/shop-demo-v2.0/DTOs/CustomerRequestDTO.cs
+++ src/shop-demo-v2.0/DTOs/CustomerRequestDTO.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using ShopDemo.Utils;
+
 namespace ShopDemo.DTOs
 {
     public class CustomerRequestDTO
     {
+        [Required(AllowEmptyStrings = false, ErrorMessage = DomainResponseMessages.CustomerFirstNameIsRequired)]
+        [MaxLength(length: CustomerConstantsRules.MAXIMUM_CHARACTERS_FIRST_NAME, ErrorMessage = DomainResponseMessages.MaximumOf40CharactersError)]
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
