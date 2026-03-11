fb7b6c7f1084492a70e676c443217ce2008dc841
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-03-11 15:34:43 -0300

    feat: Add public properties (Address class)

--- src/shop-demo-v2.0/Models/Address.cs
+++ src/shop-demo-v2.0/Models/Address.cs
@@ -2,6 +2,14 @@ namespace ShopDemo.Models
 {
     public class Address
     {
-        
+        // Public Properties
+        public string ZipCode { get; set; }
+        public string Street { get; set; }
+        public int Number { get; set; }
+        public string Neighborhood { get; set; }
+        public string AddressComplement { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
     }
 }
