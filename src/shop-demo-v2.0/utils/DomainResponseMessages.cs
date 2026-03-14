3d17991d63c02bf69e2c58f49b546723b70fce53
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-03-14 11:38:21 -0300

    feat: added OrderNotFoundMessageError message to DomainResponseMessages

--- src/shop-demo-v2.0/utils/DomainResponseMessages.cs
+++ src/shop-demo-v2.0/utils/DomainResponseMessages.cs
@@ -36,5 +36,6 @@ namespace ShopDemo.Utils
         public const string OrderIdMustBeGreaterThanZeroError = "OrderId must be greater than zero";
         public const string OrderNumberMustBeGreaterThanZeroError = "OrderNumber must be greater than zero";
         public const string OrderNumberIsRequired = "OrderNumber Is Required";
+        public const string OrderNotFoundMessageError = "Order Not Found";
     }
 }
