ca599f0261095c257ac5d3cd426a1c6ed16e6a44
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-03-19 22:43:56 -0300

    feat: Added ZipCodeIsRequired property to DomainResponseMessages

--- src/shop-demo-v2.0/utils/DomainResponseMessages.cs
+++ src/shop-demo-v2.0/utils/DomainResponseMessages.cs
@@ -22,6 +22,7 @@ namespace ShopDemo.Utils
         public const string MaximumOf100CharactersError = "Must have a maximum of 100 characters";
         public const string MaximumOf50CharactersError = "Must have a maximum of 50 characters";
         public const string MustHaveAtLeastOneAddress = "You must have at least one address";
+        public const string ZipCodeIsRequired = "ZipCode Is Required";
     
         // Product class
         public const string ProductIdMustBeGreaterThanZeroError = "ProductId must be greater than zero";
