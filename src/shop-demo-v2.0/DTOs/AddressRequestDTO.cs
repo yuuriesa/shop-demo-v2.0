5aec868126a8dbeb819127bdfa98e37a1c2a6fcd
Yuri Torres <yuri.salgado.ft@gmail.com>
2026-03-19 22:39:45 -0300

    feat: Added DataAnnotations [Required] to AddressRequestDTO

--- src/shop-demo-v2.0/DTOs/AddressRequestDTO.cs
+++ src/shop-demo-v2.0/DTOs/AddressRequestDTO.cs
@@ -1,14 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ShopDemo.DTOs
 {
     public class AddressRequestDTO
     {
+        [Required]
         public string ZipCode { get; set; }
+        [Required]
         public string Street { get; set; }
+        [Required]
         public int Number { get; set; }
+        [Required]
         public string Neighborhood { get; set; }
+        [Required]
         public string AddressComplement { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string Country { get; set; }  
     }
 }
